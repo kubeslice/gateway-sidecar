37d9549c690b0f16db377d4c36dc26b2ee1f8e1c
rahulii <r.sawra@gmail.com>
2022-02-16 13:27:03 +0530

    check if dir/file already exists and start grpc Server
    Signed-off-by: rahulii <r.sawra@gmail.com>

--- pkg/bootstrap/bootstrap.go
+++ pkg/bootstrap/bootstrap.go
@@ -1,8 +1,8 @@
 package bootstrap
 
 import (
-	"os"
 	"io"
+	"os"
 	"bitbucket.org/realtimeai/kubeslice-gw-sidecar/pkg/logger"
 )
 
@@ -29,19 +29,32 @@ func NewGatewayPod(mode string,mountPath,secretMountPath string ,logger *logger.
 
 // Process() creates a directory structure as required by openvpn pods
 func (gw *GatewayPod) Process() error {
-	baseFileName := os.Getenv("CLUSTER_ID") + os.Getenv("SLICE_NAME")
+	baseFileName := os.Getenv("CLUSTER_ID") +"-" +  os.Getenv("SLICE_NAME")
 	if gw.mode == SERVER {
-		//create two directories named pki and ccd in /mountPath (eg: /config/pki)
-		err := os.Mkdir(gw.mountPath+"/pki",0755)
+		//create two directories named pki and ccd in /mountPath (eg: /config/pki) if not exists
+		present,err := exists(gw.mountPath+"/pki")
 		if err != nil {
 			return err
-		}
-		err = os.Mkdir(gw.mountPath+"/ccd",0755)
-		if err != nil {
-			return err
+		} 
+		if !present{
+			err = os.Mkdir(gw.mountPath+"/pki",0755)
+			if err != nil {
+				return err
+			}
 		}
 
-		//create two files named openvpn.conf and copy the contents from source file in secretMountPath
+		present,err = exists(gw.mountPath+"/ccd")
+		if err != nil {
+			return err
+		} 
+		if !present {
+			err = os.Mkdir(gw.mountPath+"/ccd",0755)
+			if err != nil {
+				return err
+			}
+		}
+
+		//create a file named openvpn.conf and copy the contents from source file present at secretMountPath
 		files:= []string{"openvpn.conf"}
 
 		for _,file := range files{
@@ -53,14 +66,27 @@ func (gw *GatewayPod) Process() error {
 			}
 		}
 
-		// create sub-directories "issued" and "private"
-		err = os.Mkdir(gw.mountPath+"/pki/"+"issued",0755)
+		// create sub-directories "issued" and "private" in "/mountPath/pki"
+		present,err = exists(gw.mountPath+"/pki/"+"issued")
 		if err != nil {
 			return err
+		} 
+		if !present {
+			err = os.Mkdir(gw.mountPath+"/pki/"+"issued",0755)
+			if err != nil {
+				return err
+			}
 		}
-		err = os.Mkdir(gw.mountPath+"/pki/"+"private",0755)
+		
+		present,err = exists(gw.mountPath+"/pki/"+"private")
 		if err != nil {
 			return err
+		} 
+		if !present {
+			err = os.Mkdir(gw.mountPath+"/pki/"+"private",0755)
+			if err != nil {
+				return err
+			}
 		}
 		//TODO:Discuss on the last fileName
 		//Copy these 3 files from /secretMountPath to /mountPath/pki/
@@ -96,21 +122,46 @@ func (gw *GatewayPod) Process() error {
 	return nil
 }
 
+func exists(path string) (bool,error) {
+	_,err := os.Stat(path)
+	if err == nil {
+		return true,nil
+	}
+	if os.IsNotExist(err){
+		return false,nil
+	}
+	return false,err
+}
 func CopyFile(source string, dest string) (error) {
-	sourcefile, err := os.Open(source)
+	var sourcefile,destfile *os.File
+	var err error
+
+	sourcefile, err = os.Open(source)
 	if err != nil {
 		return err
 	}
 
 	defer sourcefile.Close()
 
-	destfile, err := os.Create(dest)
+	//check if the destination file already exists , if not create it
+	present,err := exists(dest)
 	if err != nil {
 		return err
 	}
-
-	defer destfile.Close()
-
+	if !present{
+		destfile, err := os.Create(dest)
+		if err != nil {
+			return err
+		}
+		defer destfile.Close()
+	} else {
+		destfile ,err := os.Open(dest)
+		if err != nil {
+			return err
+		}
+		defer destfile.Close()
+	}
+	
 	_, err = io.Copy(destfile, sourcefile)
 	if err == nil {
 		sourceinfo, err := os.Stat(source)
